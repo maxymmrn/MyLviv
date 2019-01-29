9ad0428f5b7d58e627ab3d05adfd2f3eb3f85722
Maxym Marina <maxymmrn@gmail.com>
2019-01-29 10:11:26 +0200

    commit for Oleh

--- app/src/main/java/com/android/mylviv/MainActivity.java
+++ app/src/main/java/com/android/mylviv/MainActivity.java
@@ -1,5 +1,6 @@
 package com.android.mylviv;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -12,6 +13,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity
         implements NavigationView.OnNavigationItemSelectedListener {
@@ -40,6 +42,17 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Button myOrders = (Button) findViewById(R.id.btn);
+
+        myOrders.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent OpenListOfOrders = new Intent(MainActivity.this, complain_status.class);
+                startActivity(OpenListOfOrders);
+            }
+        });
+
     }
 
     @Override
---
+++ app/src/main/java/com/android/mylviv/complain_status.java
@@ -0,0 +1,13 @@
+package com.android.mylviv;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class complain_status extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_complain_status);
+    }
+}
